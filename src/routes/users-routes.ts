a25ddfbf6088da34f56398756079e570284555d7
Hari prasad P <hariprasad.1si22cs404@gmail.com>
2025-03-26 10:18:11 +0530

    update user retrieval route to improve error handling and response structure

--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -1,8 +1,8 @@
 import { Hono } from "hono";
 import { prismaClient } from "../extras/prisma";
 import { tokenMiddleware } from "./middlewares/token-middlewares";
+import { getAllUsers, getMe } from "../controllers/users/users-controller";
 import { GetMeError } from "../controllers/users/user-type";
-import { getMe } from "../controllers/users/users-controller";
 
 export const usersRoutes = new Hono();
 
@@ -39,15 +39,23 @@ usersRoutes.get("/me", tokenMiddleware, async (context) => {
   }
 });
 
-usersRoutes.get("/all", tokenMiddleware, async (c) => {
+// /users
+usersRoutes.get("", tokenMiddleware, async (context) => {
   try {
-    const users = await getAllUser();
-    return c.json(users, 200);
-  } catch (e) {
-    if (e === GetAllUserError.UNKNOWN) {
-      return c.json({ error: "Unknown error" }, 500);
-    }
+    const users = await getAllUsers();
 
-    return c.json({ message: "Internal Server Error" }, 500);
+    return context.json(
+      {
+        data: users,
+      },
+      200
+    );
+  } catch (e) {
+    return context.json(
+      {
+        message: "Internal Server Error",
+      },
+      500
+    );
   }
 });
